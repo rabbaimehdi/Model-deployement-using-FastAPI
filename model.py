a24df8020959c071700a37240b4c14de8ae0b7f0
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2023-01-10 00:01:23 +0000

    develop model.py

--- model.py
+++ model.py
@@ -4,3 +4,35 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
+diabetes_dataset = pd.read_csv('./diabetes.csv') 
+#print(diabetes_dataset.head())
+
+# Model Dev
+
+X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+
+#print(X.shape, X_train.shape, X_test.shape)
+
+classifier = svm.SVC(kernel='linear')   
+classifier.fit(X_train, Y_train)
+
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy score of the training data : ', training_data_accuracy)
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+
+print('Accuracy score of the test data : ', test_data_accuracy)
+
+#Model Deploy
+
+import pickle
+
+filename = 'diabetes_model.pickle'
+pickle.dump(classifier, open(filename, 'wb'))
+
+
